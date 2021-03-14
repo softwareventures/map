fca06139a0332fbf5796f9f0991711f31da84f6a
Daniel Cassidy <mail@danielcassidy.me.uk>
2021-03-14 06:14:30 +0000

    feat(fromfirstentries): add fromFirstEntries function

--- index.test.ts
+++ index.test.ts
@@ -1,3 +1,18 @@
 import test from "ava";
+import {fromFirstEntries} from "./index";
 
-test("placeholder test", t => t.true(true));
+test("fromFirstEntries", t => {
+    t.deepEqual(
+        [
+            ["a", 1],
+            ["b", 2]
+        ],
+        Array.from(
+            fromFirstEntries([
+                ["a", 1],
+                ["b", 2],
+                ["a", 3]
+            ])
+        )
+    );
+});
--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
 export type MapLike<TKey, TValue> = Iterable<[TKey, TValue]>;
+
+export function fromFirstEntries<TKey, TValue>(entries: MapLike<TKey, TValue>): Map<TKey, TValue> {
+    const map = new Map<TKey, TValue>();
+    for (const [key, value] of entries) {
+        if (!map.has(key)) {
+            map.set(key, value);
+        }
+    }
+    return map;
+}
