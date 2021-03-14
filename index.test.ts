50e27165af1404f84fcb6d061325533b3bbc64a7
Daniel Cassidy <mail@danielcassidy.me.uk>
2021-03-14 07:20:33 +0000

    feat(fromallentries): add fromAllEntries function

--- index.test.ts
+++ index.test.ts
@@ -1,5 +1,5 @@
 import test from "ava";
-import {fromFirstEntries, fromLastEntries} from "./index";
+import {fromAllEntries, fromFirstEntries, fromLastEntries} from "./index";
 
 test("fromFirstEntries", t => {
     t.deepEqual(
@@ -29,6 +29,22 @@ test("fromLastEntries", t => {
         [
             ["a", 3],
             ["b", 2]
+        ]
+    );
+});
+
+test("fromAllEntries", t => {
+    t.deepEqual(
+        Array.from(
+            fromAllEntries([
+                ["a", 1],
+                ["b", 2],
+                ["a", 3]
+            ])
+        ),
+        [
+            ["a", [1, 3]],
+            ["b", [2]]
         ]
     );
 });
--- index.ts
+++ index.ts
@@ -13,3 +13,15 @@ export function fromFirstEntries<TKey, TValue>(entries: MapLike<TKey, TValue>): 
 export function fromLastEntries<TKey, TValue>(entries: MapLike<TKey, TValue>): Map<TKey, TValue> {
     return new Map(entries);
 }
+
+export function fromAllEntries<TKey, TValue>(entries: MapLike<TKey, TValue>): Map<TKey, TValue[]> {
+    const map = new Map<TKey, TValue[]>();
+    for (const [key, value] of entries) {
+        const values = map.get(key) ?? [];
+        values.push(value);
+        if (!map.has(key)) {
+            map.set(key, values);
+        }
+    }
+    return map;
+}
