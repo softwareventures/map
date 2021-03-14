a25cc8e69f676867a864c9a024164caa05fd83f9
Daniel Cassidy <mail@danielcassidy.me.uk>
2021-03-14 07:19:31 +0000

    test: fix swapped expected and actual values

--- index.test.ts
+++ index.test.ts
@@ -3,32 +3,32 @@ import {fromFirstEntries, fromLastEntries} from "./index";
 
 test("fromFirstEntries", t => {
     t.deepEqual(
-        [
-            ["a", 1],
-            ["b", 2]
-        ],
         Array.from(
             fromFirstEntries([
                 ["a", 1],
                 ["b", 2],
                 ["a", 3]
             ])
-        )
+        ),
+        [
+            ["a", 1],
+            ["b", 2]
+        ]
     );
 });
 
 test("fromLastEntries", t => {
     t.deepEqual(
-        [
-            ["a", 3],
-            ["b", 2]
-        ],
         Array.from(
             fromLastEntries([
                 ["a", 1],
                 ["b", 2],
                 ["a", 3]
             ])
-        )
+        ),
+        [
+            ["a", 3],
+            ["b", 2]
+        ]
     );
 });
